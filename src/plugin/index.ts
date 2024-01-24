3ae7731d276be4a3571d717bc8498559fe66dcb5
Alex Guerra <alexander.guerra@braze.com>
2024-01-23 20:16:50 -0600

    Use in-memory language service

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,22 +1,23 @@
 import { PluginStrictFileChecker } from './PluginStrictFileChecker';
-import { log, PluginInfo, setupProxy, turnOffStrictMode, turnOnStrictMode } from './utils';
+import { log, PluginInfo, setupProxy } from './utils';
 import * as ts from 'typescript/lib/tsserverlibrary';
 
 const init: ts.server.PluginModuleFactory = () => {
   function create(info: PluginInfo) {
     const proxy = setupProxy(info);
+    const strictLanguageServiceHost = proxyHost(info);
+    const strictLanguageService = ts.createLanguageService(strictLanguageServiceHost);
+
     log(info, 'Plugin initialized');
 
     proxy.getSemanticDiagnostics = function (filePath) {
       const strictFile = new PluginStrictFileChecker(info).isFileStrict(filePath);
 
       if (strictFile) {
-        turnOnStrictMode(info);
+        return strictLanguageService.getSemanticDiagnostics(filePath);
       } else {
-        turnOffStrictMode(info);
+        return info.languageService.getSemanticDiagnostics(filePath);
       }
-
-      return info.languageService.getSemanticDiagnostics(filePath);
     };
 
     return proxy;
@@ -24,5 +25,44 @@ const init: ts.server.PluginModuleFactory = () => {
 
   return { create };
 };
+
+function proxyHost(info: PluginInfo): ts.LanguageServiceHost {
+  const proxy: ts.LanguageServiceHost = Object.create(null);
+  const host = info.languageServiceHost;
+  const keys = Object.keys(host);
+  // Bunch of methods that are needed but aren't own properties.
+  // Not sure of a better way to handle this.
+  keys.push(
+    'getCurrentDirectory',
+    'getCompilationSettings',
+    'getCompilerHost',
+    'fileExists',
+    'readFile',
+    'trace',
+    'directoryExists',
+    'realpath',
+    'getCurrentDirectory',
+    'getDirectories',
+    'useCaseSensitiveFileNames',
+    'getScriptFileNames',
+    'getDefaultLibFileName',
+    'getScriptSnapshot',
+    'getScriptVersion',
+  );
+  for (const k of keys as Array<keyof ts.LanguageServiceHost>) {
+    const serviceFunction = host[k];
+    // @ts-ignore
+    proxy[k] = (...args: Array<unknown>) => {
+      // @ts-ignore
+      return serviceFunction!.apply(host, args);
+    };
+  }
+  proxy.getCompilationSettings = () => {
+    const result = host.getCompilationSettings!.apply(host);
+    return { ...result, strict: true };
+  };
+
+  return proxy;
+}
 
 module.exports = init;
--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -3,14 +3,6 @@ import { PLUGIN_NAME } from '../common/constants';
 
 export type PluginInfo = ts.server.PluginCreateInfo;
 
-export function turnOnStrictMode(info: PluginInfo): void {
-  info.project['compilerOptions'].strict = true;
-}
-
-export function turnOffStrictMode(info: PluginInfo): void {
-  info.project['compilerOptions'].strict = false;
-}
-
 export function setupProxy(info: PluginInfo) {
   const proxy: ts.LanguageService = Object.create(null);
   for (const k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
